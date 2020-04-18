1c1baba0eaf80fe335e9229659dbc87dddbb411b
Patryk <patryk.baryla@spoko.pl>
2020-04-18 15:10:04 +0200

    tweaks

--- camera/Camera.py
+++ camera/Camera.py
@@ -38,6 +38,10 @@ class camera:
     def __init__(self):
         self.histogram_created_check = False
 
+        self.histogram = None
+        self.cap = cv2.VideoCapture(0)
+        self.last_max_contour = None
+
     def draw_place(self, frame):
         rows, cols, _ = frame.shape
         self.first_place = [(int(9.5 * cols / 20), int(9.5 * rows / 20)),
@@ -56,7 +60,7 @@ class camera:
         histogram = cv2.calcHist([hsv_frame], [0, 1], None, [180, 256], [0, 180, 0, 256])
 
         return cv2.normalize(histogram, histogram, 0, 255, cv2.NORM_MINMAX), hsv_frame
-
+    """
     def capture(self):
         cap = cv2.VideoCapture(0)
         cv2.namedWindow('Life', cv2.WINDOW_NORMAL)
@@ -76,7 +80,7 @@ class camera:
 
             if self.histogram_created_check:
                 hist_masked_image = masking_histogram(frame, histogram)
-                erode_kernel = np.ones((15, 15), np.uint8)
+                erode_kernel = np.ones((5, 5), np.uint8)
                 dilate_kernel = np.ones((5, 5), np.uint8)
                 hist_masked_image = cv2.erode(hist_masked_image, erode_kernel)
                 hist_masked_image = cv2.dilate(hist_masked_image, dilate_kernel)
@@ -97,11 +101,69 @@ class camera:
 
         cap.release()
         cv2.destroyAllWindows()
+    """
+
+    def scan_object(self):
+        cv2.namedWindow('Scan', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Scan', 800, 600)
+
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+
+            frame = cv2.flip(frame, 1)
+
+            if self.histogram_created_check is False:
+                frame = self.draw_place(frame)
+
+            cv2.imshow('Scan', frame)
+
+            if cv2.waitKey(1) & 0xFF == 32:
+                self.histogram_created_check = True
+                self.histogram, _ = self.create_histogram(frame)
+                break
+        cv2.destroyAllWindows()
+        return self.histogram
+
+    def get_center(self):
+        ret, frame = self.cap.read()
+
+        frame = cv2.flip(frame, 1)
+
+        if self.histogram_created_check:
+            hist_masked_image = masking_histogram(frame, self.histogram)
+            erode_kernel = np.ones((5, 5), np.uint8)
+            dilate_kernel = np.ones((5, 5), np.uint8)
+            hist_masked_image = cv2.erode(hist_masked_image, erode_kernel)
+            hist_masked_image = cv2.dilate(hist_masked_image, dilate_kernel)
+            contour_list = contours(hist_masked_image)
+            try:
+                max_cont = max(contour_list, key=cv2.contourArea)
+            except ValueError:
+                print("out")
+                max_cont = self.last_max_contour
+            cnt_centroid = centroid(max_cont)
+            cv2.circle(frame, cnt_centroid, 5, [255, 0, 255], -1)
+        else:
+            frame = self.draw_place(frame)
+
+        return frame, cnt_centroid
 
 
 def main():
     usage = camera()
-    usage.capture()
+    # usage.capture()
+
+    cv2.namedWindow('Live', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Live', 800, 600)
+    usage.scan_object()
+    while usage.cap.isOpened():
+        img, _ = usage.get_center()
+        cv2.imshow('live', img)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    usage.cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
