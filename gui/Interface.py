d15ff35c022b04241ece8ae16ad86e386e76f399
Aoxter <pkopycki@onet.pl>
2020-04-25 14:14:22 +0200

    GUI tweaks
    Layout manager changed from pack to grid.

--- gui/Interface.py
+++ gui/Interface.py
@@ -1,6 +1,7 @@
 #from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 class ToolTip(object):
@@ -49,6 +50,13 @@ class Application(tk.Frame):
         self.IMAGES['redColour'] = redColour
         #self.IMAGES['greenColour'] = greenColourIcon
 
+    def resize(self, event):
+        size = (event.width, event.height)
+        resized = self.original_frame.resize(size, Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(resized)
+        self.display.delete("IMG")
+        self.display.create_image(0, 0, image=self.image, anchor=tk.NW, tags="IMG")
+
     def createToolTip(self, widget, text="Temp"):
         toolTip = ToolTip(widget)
 
@@ -63,7 +71,7 @@ class Application(tk.Frame):
 
     def createWidgets(self):
         #toplevel menu
-        self.MENU = tk.Menu(self.master)
+        self.MENU = tk.Menu(self)
 
         #file menu
         self.FILEMENU = tk.Menu(self.MENU)
@@ -73,9 +81,9 @@ class Application(tk.Frame):
         self.FILEMENU.add_command(label='Save')
         self.FILEMENU.add_command(label='Save as')
         self.FILEMENU.add_separator()
-        self.FILEMENU.add_command(label='Exit', command=self.master.quit)
+        self.FILEMENU.add_command(label='Exit', command=self.quit)
         # display the menu
-        self.master.config(menu=self.MENU)
+        self.parent.config(menu=self.MENU)
 
         #edit menu
         self.EDITMENU = tk.Menu(self.MENU)
@@ -94,17 +102,23 @@ class Application(tk.Frame):
         self.SETTINGSMENU.add_command(label='Not implemented yet')
 
         #left frame for tools
-        self.TOOLSFRAME = tk.Frame(self.master, width=100)
-        self.TOOLSFRAME.pack(anchor=tk.N, fill=tk.BOTH, expand=False, side=tk.LEFT)
+        self.TOOLSFRAME = tk.Frame(self, width=100)
+        self.TOOLSFRAME.grid(row=0, column=0, sticky=tk.W)
+        #(anchor=tk.N, fill=tk.BOTH, expand=False, side=tk.LEFT)
 
         #central frame for image
-        self.IMAGEFRAME = tk.Frame(self.master, background="White")
-        self.IMAGEFRAME.pack(fill=tk.BOTH, expand=True)
+        self.IMAGEFRAME = tk.Frame(self, width=540, height=380)
+        self.IMAGEFRAME.grid(row=0, column=1)
+        #(fill=tk.BOTH, expand=True)
 
+        #bottom frame - TODO empty space to use
+        self.BOTTOMFRAME = tk.Frame(self, height=100)
+        self.BOTTOMFRAME.grid(row=1, column=1)
         #tools widgets
         #Creating button
         self.TOOLBUTTON = tk.Menubutton(self.TOOLSFRAME, bd=0, image=self.IMAGES['brush'], compound=tk.CENTER)
-        self.TOOLBUTTON.pack(side=tk.TOP)
+        self.TOOLBUTTON.grid(row=0, column=0, pady=2)
+        #.pack(side=tk.TOP)
         #Creating tools menu
         self.TOOLBUTTON.menu = tk.Menu(self.TOOLBUTTON, tearoff=0)
         self.TOOLBUTTON["menu"] = self.TOOLBUTTON.menu
@@ -116,7 +130,8 @@ class Application(tk.Frame):
 
         # Creating button
         self.COLOURBUTTON = tk.Menubutton(self.TOOLSFRAME, bd=0, image=self.IMAGES['redColour'], compound=tk.CENTER)
-        self.COLOURBUTTON.pack(side=tk.TOP)
+        self.COLOURBUTTON.grid(row=1, column=0, pady=2)
+        #pack(side=tk.TOP)
         # Creating colour menu
         self.COLOURBUTTON.menu = tk.Menu(self.COLOURBUTTON, tearoff=0)
         self.COLOURBUTTON["menu"] = self.COLOURBUTTON.menu
@@ -126,6 +141,15 @@ class Application(tk.Frame):
         self.COLOURBUTTON.menu.add_command(label='', underline=0, image=self.IMAGES['redColour'])
         #ToolTip for button
         self.createToolTip(self.COLOURBUTTON, "Colour")
+
+        #Creating label with background for image grid
+        self.original_frame = Image.open('bird.jpg')
+        self.frame = ImageTk.PhotoImage(self.original_frame)
+
+        self.display = tk.Canvas(self.IMAGEFRAME, bd=0, highlightthickness=0)
+        self.display.create_image(0, 0, image=self.frame, anchor=tk.NW, tags="IMG")
+        self.display.grid(row=0, sticky=tk.W + tk.E + tk.N + tk.S)
+        self.IMAGEFRAME.bind("<Configure>", self.resize)
 
         #tutorial buttons
         # self.QUIT = tk.Button(self)
@@ -141,16 +165,35 @@ class Application(tk.Frame):
         #
         # self.hi_there.pack({"side": "left"})
 
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        #self.pack()
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.initGui()
+
+    def initGui(self):
+        self.parent.title("Camera Paint")
+        self.parent.geometry('640x480')
+        self.parent.resizable(width=tk.TRUE, height=tk.TRUE)
+        self.grid(sticky=tk.W + tk.E + tk.N + tk.S, padx=20, pady=20)
+        #self.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+
+        # self.parent.columnconfigure(0, weight=1)
+        # self.parent.rowconfigure(1, weight=1)
+        # self.columnconfigure(0, weight=1)
+        # self.rowconfigure(1, weight=1)
+
+        self.rowconfigure(0, weight=10)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=10)
         self.IMAGES = {}
         self.initializeImages()
         self.createWidgets()
 
 root = tk.Tk()
-root.title('Camera Paint')
-root.geometry('640x480')
-app = Application(master=root)
+# root.title('Camera Paint')
+# root.geometry('640x480')
+app = Application(parent=root)
+app.pack(fill="both", expand=True)
 app.mainloop()
 root.destroy()
